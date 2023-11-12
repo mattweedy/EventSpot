905dc731ba49cbd05088333ee2537c3247c28a0f
mattweedy <matthewtweedy02@gmail.com>
2023-11-12 23:18:12 +0000

    defined plan of action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,39 @@
 import "./App.css";
 import Axios from 'axios';
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 function App() {
   const items = [
-    {id: 1, name: 'draw up ( figma ) designs of what app looks like'},
-    {id: 2, name: 'get api pulling'},
-    {id: 3, name: 'get api displaying'},
-    {id: 4, name: 'get api storing'},
-    {id: 5, name: 'continue research on react etc'},
-    {id: 6, name: 'begin research on machine learning / clustering / closest event first'}
+    { id: 1, name: 'draw up ( figma ) designs of what app looks like' },
+    { id: 2, name: 'continue research on react etc' },
+    { id: 3, name: 'begin research on machine learning / clustering / closest event first' },
+    { id: 4, name: 'create landing page - this week 2' },
+    { id: 5, name: 'create base recommender [use sample data / generate sample data via mongodb] - week 3 & 4' },
   ];
 
-  const[comments,setComments]=useState([])
+  const week2 = [
+    { id: 1, name: 'quiz questions' },
+    { id: 2, name: 'diagrams of database design', detail: {
+                                                            first: 'users',
+                                                            second: 'events',
+                                                            third: 'other?'
+                                                          } },
+    { id: 3, name: 'wireframes of webapp', detail: {
+                                                    first: 'landing page',
+                                                    second: 'quiz page',
+                                                    third: 'login/signup',
+                                                    fourth: 'events/rec page'
+                                                  } },
+    { id: 4, name: 'use case diagrams', detail: { first: 'refer to wireframes for idea' } }
+  ]
+
+  const laterItems = [
+    { id: 1, name: 'get api pulling' },
+    { id: 2, name: 'get api displaying' },
+    { id: 3, name: 'get api storing' },
+  ]
+
+  const [comments, setComments] = useState([])
+
   useEffect(() => {
     fetchComments();
   }, [])
@@ -19,7 +41,7 @@ function App() {
     console.log(comments)
   }, [comments])
 
-  const fetchComments = async() => {
+  const fetchComments = async () => {
     const response = await Axios('http://jsonplaceholder.typicode.com/comments');
     setComments(response.data)
   }
@@ -27,16 +49,27 @@ function App() {
   return (
     <div className="App">
       <h1>Todo List</h1>
-
-      <ul style={{alignItems:'left'}}>
+      <ul>
         {items.map((item) => <li key={item.id}>{item.name}</li>)}
       </ul>
 
+      <h2>Week 2</h2>
+      <ul>
+        {week2.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
+
+      <h2>Week 3/4</h2>
+      <ul>
+        {laterItems.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
+
+
       <h2>Json Placeholder API - Comments</h2>
       {
-        comments && comments.map(comment => {
-          return(
-            <div key={comment.id} style={{alignItems:'center',margin:'20px 60px'}}>
+        // check comments exists before displaying each (sliced to display first 15)
+        comments && comments.slice(0, 15).map(comment => {
+          return (
+            <div key={comment.id} style={{ alignItems: 'center', margin: '20px 60px' }}>
               <h4>{comment.name}</h4>
               <p>{comment.body}</p>
               <p>{comment.email}</p>

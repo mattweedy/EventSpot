0c87644ad902396fd3dbfdb16ba3a44609c1239f
mattweedy <matthewtweedy02@gmail.com>
2023-11-12 22:23:30 +0000

    displays comments from jsonplaceholderAPI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import "./App.css";
+import Axios from 'axios';
+import React,{useState,useEffect} from 'react';
 function App() {
   const items = [
     {id: 1, name: 'draw up ( figma ) designs of what app looks like'},
@@ -8,13 +11,40 @@ function App() {
     {id: 6, name: 'begin research on machine learning / clustering / closest event first'}
   ];
 
+  const[comments,setComments]=useState([])
+  useEffect(() => {
+    fetchComments();
+  }, [])
+  useEffect(() => {
+    console.log(comments)
+  }, [comments])
+
+  const fetchComments = async() => {
+    const response = await Axios('http://jsonplaceholder.typicode.com/comments');
+    setComments(response.data)
+  }
+
   return (
-    <>
+    <div className="App">
       <h1>Todo List</h1>
-      <ul>
+
+      <ul style={{alignItems:'left'}}>
         {items.map((item) => <li key={item.id}>{item.name}</li>)}
       </ul>
-    </>
+
+      <h2>Json Placeholder API - Comments</h2>
+      {
+        comments && comments.map(comment => {
+          return(
+            <div key={comment.id} style={{alignItems:'center',margin:'20px 60px'}}>
+              <h4>{comment.name}</h4>
+              <p>{comment.body}</p>
+              <p>{comment.email}</p>
+            </div>
+          )
+        })
+      }
+    </div>
   );
 }
 

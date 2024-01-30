db5b5078e0163883f69098fe63db402a0cd5408c
mattweedy <matthewtweedy02@gmail.com>
2024-01-30 21:56:51 +0000

    Update models.py and manage.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,7 +7,9 @@ class User(models.Model):
     # add more as needed
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    venue = models.CharField(max_length=200)
     date = models.DateTimeField()
+    price = models.CharField(max_length=50)
     genre = models.CharField(max_length=100)
     # add more as needed

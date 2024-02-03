d6f27afac9b450403fba2abcab8c5eadaaa2faa6
mattweedy <matthewtweedy02@gmail.com>
2024-02-03 21:18:57 +0000

    Refactor Playwright scraper to use async API, Began parsing HTML for event details. In future change for API JSON data if possible

--- backend/spotevent/playwright_scraper.py
+++ backend/spotevent/playwright_scraper.py
@@ -1,8 +1,9 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
+# from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 from pydantic_models import ResponseModel
 import json
-import asyncio
 
 def event_data_get(page):
     # i want to select the <ul> tag with classname="search-main-content__events-list" and then select all <li> tags
@@ -14,12 +15,36 @@ async def run(p) -> None:
     context = await browser.new_context()
     page = await context.new_page()
     await page.goto("https://www.eventbrite.com/d/ireland--dublin/music--performances/")
+    # event_details = "li div div+div section div section+section"
+    # event_price_details = 'li > div > div.discover-search-desktop-card.discover-search-desktop-card--hiddeable > section > div > section.event-card-details > div > div > p'
+    # await page.is_visible(event_price_details)
+    
+    # get list of events in html
+    html = await page.inner_html('ul.search-main-content__events-list')
+    soup = BeautifulSoup(html, 'html.parser')
+    # print(soup.find_all('p'))
     
     # click the next page button for as many pages of results there are
     while True:
         try:
-            next_button = page.wait_for_selector("button[data-spec='page-next']")
-            next_button.click()
+            events = soup.find_all('li')
+            # print(events)
+
+            # events = await page.query_selector_all('li div div+div section div section+section')
+            for e in events:
+                scraped_event = {}
+                name = events.find_all('h2')
+                print(name).text
+
+                # event details
+                # name = await e.query_selector('h2')
+                # name = await e.query_selector('li > div > div.discover-search-desktop-card.discover-search-desktop-card--hiddeable > section > div > section.event-card-details > div > a > h2')
+                # venue = await e.query_selector('p')
+                # date = await e.query_selector('p')
+                # link = await e.query_selector('div.Stack_root__1ksk7 a')
+
+            next_button = await page.wait_for_selector("button[data-spec='page-next']")
+            await next_button.click()
         except:
             break
 
@@ -28,7 +53,7 @@ async def run(p) -> None:
 
 
 async def main() -> None:
-    async with sync_playwright() as p:
+    async with async_playwright() as p:
         await run(p)
 
 

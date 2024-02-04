fdd49461dcea7782ca502fb3a091cb25c3ef8a15
mattweedy <matthewtweedy02@gmail.com>
2024-02-04 03:01:02 +0000

    Refactor event_data_get function and run function

--- backend/spotevent/playwright_scraper.py
+++ backend/spotevent/playwright_scraper.py
@@ -1,55 +1,59 @@
-import asyncio
-from playwright.async_api import async_playwright
-# from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 from pydantic_models import ResponseModel
 import json
 
-async def event_data_get(soup):
+def event_data_get(soup):
     # select the json inside each script tag of type="application/ld+json" with beautiful soup passed from run() DO NOT USE BY CSS SELECTOR
     script_tags = soup.find_all('script', type="application/ld+json")
-    for script in script_tags:
-        # load the json from the script tag
-        event_info = json.loads(script.string)
-        # check if the json contains the event key
-        if "event" in event_info:
-            # get the event number from the json
-            event_number = event_info["event"]["id"]
-            print(event_number)
-            return event_number
-        else:
-            print("No event found")
-            return None
+
+    print(script_tags.inner_html)
+    # for script in script_tags:
+    #     # load the json from the script tag
+    #     event_info = json.loads(script.string)
+    #     # check if the json contains the event key
+    #     if "url" in event_info:
+    #         # get the event number from the json
+    #         eventbrite_event_id = 
+    #         event_number = event_info["event"]["id"]
+    #         print(event_number)
+    #         return event_number
+    #     else:
+    #         print("No event found")
+    #         return None
 
 
-async def run(p) -> None:
-    browser = await p.chromium.launch(headless=False)
-    context = await browser.new_context()
-    page = await context.new_page()
-    await page.goto("https://www.eventbrite.com/d/ireland--dublin/music--performances/")
+def run(p):
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.eventbrite.com/d/ireland--dublin/music--performances/")
     
     # get list of events in html
-    html = await page.inner_html('body')
+    html = page.inner_html('body')
     soup = BeautifulSoup(html, 'html.parser')
     
     # click the next page button for as many pages of results there are
     while True:
         try:
-            data = await event_data_get(soup)
+            data = event_data_get(soup)
 
-
-            next_button = await page.wait_for_selector("button[data-spec='page-next']")
-            await next_button.click()
+            # uncomment when data is secure - don't spam requests
+            # next_button = page.wait_for_selector("button[data-spec='page-next']", timeout=5000)
+            # if next_button:
+            #     next_button.click()
+            # else:
+            #     break
         except:
             break
 
-    await context.close()
-    await browser.close()
+    context.close()
+    browser.close()
 
 
-async def main() -> None:
-    async with async_playwright() as p:
-        await run(p)
+def main():
+    with sync_playwright() as p:
+        run(p)
 
 
-asyncio.run(main())
+main()

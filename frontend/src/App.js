e1303406f8b8c9f357e3acbde7c1e03280179be5
mattweedy <matthewtweedy02@gmail.com>
2024-02-08 13:55:00 +0000

    Displaying event and venue now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [],
+      venues: []
     };
   }
 
-  componentDidMount() {
-    let data;
-    axios.get('http://localhost:8000/events/')
+  fetchData(endpoint) {
+    axios.get(`http://localhost:8000${endpoint}`)
       .then(res => {
-        data = res.data.results;
-        console.log("data : ", data);
+        const data = res.data.results;
+        console.log(endpoint, "data:", data);
         this.setState({
-           events: data
+          [endpoint.slice(1, -1)]: data
         });
       })
-      .catch(err => {console.log(err)})
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  // componentDidMount() {
+  //   this.fetchData('/events/');
+  //   this.fetchData('/venues/');
+  // }
+
+  componentDidMount() {
+    let e_data;
+    let v_data;
+    e_data = this.fetchData('/events/');
+    v_data = this.fetchData('/venues/');
   }
 
   render() {
@@ -31,15 +45,24 @@ class App extends React.Component {
     return (
       <div>
         <h1>Event Data Generated From Django</h1>
-        {/* {console.log(this.state.events)} */}
-        {this.state.events.map(event_data => (
-          <div key={event_data.id}>
+        {this.state.events.map(output => (
+          <div key={output.id}>
             <hr></hr>
-            <img src={event_data.image} style={{maxWidth: "50%"}}></img>
-            <h2>{event_data.name}</h2>
-            <p>{event_data.event_id}</p>
-            <p>{event_data.price}</p>
-            <a href={event_data.tickets_url}>tickets</a>
+            <img src={output.image} style={{maxWidth: "50%"}}></img>
+            <h2>{output.name}</h2>
+            <p>{output.event_id}</p>
+            <p>{output.price}</p>
+            <p>{output.summary}</p>
+            <a href={output.tickets_url}>tickets</a>
+          </div>
+        ))}
+        {this.state.venues.map(output => (
+          <div key={output.id}>
+            <hr></hr>
+            <h2>{output.name}</h2>
+            <p>{output.venue_id}</p>
+            <p>{output.address}</p>
+            <p>{output.summary}</p>
           </div>
         ))}
       </div>

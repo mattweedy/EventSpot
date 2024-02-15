ccea3c8f234d92277fd00b486cf1e613fe2f230f
mattweedy <matthewtweedy02@gmail.com>
2024-02-15 15:26:45 +0000

    changes made

--- backend/core/scraper.py
+++ backend/core/scraper.py
@@ -5,21 +5,15 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.spotevent.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
-# from django.conf import settings
-# settings.configure()
-# django.setup()
 
 from playwright.async_api import async_playwright
-# from pydantic_models import ResponseModel
-# from pydantic_models_short import ResponseModel
 from models import Event, Venue
 from bs4 import BeautifulSoup
 from rich import print
-from asgiref.sync import async_to_sync, sync_to_async
 import requests
 import json
 
-EVENT_URL = "https://eventbrite.com/d/ireland--dublin/music--performances/"
+EVENT_URL = "https://eventbrite.com/d/ireland--dublin/music--performances/?page=2"
 EVENT_DATA_GET_URL = "https://eventbrite.com/api/v3/destination/events/?event_ids={}&page_size=20&expand=event_sales_status,image,primary_venue,ticket_availability,primary_organizer"
 NEXT_BUTTON_SELECTOR = "button[data-spec='page-next']"
 
@@ -32,14 +26,12 @@ async def navigate_to_page(context, url):
     return page
 
 
-def extract_event_ids(soup):
+async def extract_event_ids(soup):
     """
     Extract event IDs from a BeautifulSoup object.
     """
     event_ids_list = []
     script_tags = soup.find_all('script', type="application/ld+json")
-
-    # TODO: continue JWR video "still the best way to scrape data." 24:35 / 41:00
 
     for script in script_tags:
         try:
@@ -54,9 +46,8 @@ def extract_event_ids(soup):
     return event_ids_list
 
 
-# TODO: split the data into Event and Venue models
-# TODO: decide whether to do this in the scraper or in other backend file
 # TODO: save to Postgres
+# TODO: decide whether to do this in the scraper or in other backend file
 async def event_data_get(event_ids):
     """
     Get event data from Eventbrite API.
@@ -66,24 +57,30 @@ async def event_data_get(event_ids):
     url = EVENT_DATA_GET_URL.format(event_ids_str)
     response = requests.get(url)
     # TODO: DEBUGGING
-    # print("response.text : ",response.text)
-    data = json.loads(response.text)
+    # 429 error back
+    print("response.text : ",response.text)
+    try:
+        data = json.loads(response.text)
+    except json.decoder.JSONDecodeError:
+        print("Error decoding JSON.")
+        return
     venues = {}
     events = {}
 
-    for event in data["events"]:
+    for event, num in enumerate(data["events"]):
         new_venue = Venue(event)
+        new_venue.to_csv(f'backend/spotevent/data/events-csv/venue{num}.csv')
         if new_venue.venue_id not in venues:
             venues[new_venue.venue_id] = new_venue
             print(new_venue.venue_id)
             new_venue.save()
     
         new_event = Event(event)
+        new_event.to_csv(f'backend/spotevent/data/events-csv/event{num}.csv')
         if new_event.event_id not in events:
             events[new_event.event_id] = new_event
             print(new_event.event_id)
             new_event.save()
-
 
     return (events, venues)
 
@@ -100,41 +97,47 @@ async def run(p):
 
     # click the next page button for as many pages of results there are
     while True:
-        # try:
-        # get list of events from current page's html
-        html = await page.inner_html('body')
-        soup = BeautifulSoup(html, 'html.parser')
-        # then go to next page and repeat
-        event_ids = extract_event_ids(soup)
-        print(event_ids)
-        events, venues = await event_data_get(event_ids)
+        try:
+            # get list of events from current page's html
+            html = await page.inner_html('body')
+            soup = BeautifulSoup(html, 'html.parser')
+            # then go to next page and repeat
+            event_ids = await extract_event_ids(soup)
+            print(event_ids)
+            try:
+                events, venues = await event_data_get(event_ids)
+            except TypeError:
+                print("Error occurred while getting event and venue data.")
+                await context.close()
+                await browser.close()
+                break
+            print(events)
+            print(venues)
+            # break
+            # TODO: getting Response 429 from Eventbrite API - TOO MANY REQUESTS
 
-        break
-        # TODO: getting Response 429 from Eventbrite API - TOO MANY REQUESTS
+            # uncomment when data is secure - don't spam requests
+            next_button = page.wait_for_selector(NEXT_BUTTON_SELECTOR)
+            if next_button:
+                next_button.click()
+                page.wait_for_timeout(20000)
+        except Exception as e:
+            print("Error occurred. Exiting.")
+            print(e)
+            break
 
-        # uncomment when data is secure - don't spam requests
-        # next_button = page.wait_for_selector(NEXT_BUTTON_SELECTOR)
-        # if next_button:
-        #     next_button.click()
-        #     page.wait_for_timeout(20000)
-        # else:
-        #     break
-        # except Exception as e:
-        #     print("Error occurred. Exiting.")
-        #     print(e)
-        #     break
-
-    context.close()
-    browser.close()
+    await context.close()
+    await browser.close()
 
 
 async def main():
+    """
+    main function to run the scraper
+    """
     async with async_playwright() as p:
         await run(p)
 
 
 if __name__ == "__main__":
-    # import django
-    # django.setup()
     import asyncio
     asyncio.run(main())

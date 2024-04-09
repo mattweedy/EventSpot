62e7f173d39765bd86145b17e838f80bd9f21660
mattweedy <matthewtweedy02@gmail.com>
2024-04-09 12:38:35 +0100

    removed old Sidebar2.js

--- frontend/src/components/General/Loading.js
+++ frontend/src/components/General/Loading.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            {/* Add your loading animation or message here */}
             <h1 style={{ justifyContent: 'center', alignContent: 'center', marginTop: '150px' }}>Loading...</h1>
         </div>
     );
